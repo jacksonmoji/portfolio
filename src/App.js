aa1a6c81fd0c8f3251bd0d1af9671210d453fff7
jacksonmoji <jacksonmoji@gmail.com>
2020-06-23 04:00:31 +0200

    added splash screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import Loader from "./Components/Loader";
+import Loader from "./Components/Loader";
 import About from './assets/Components/About';
 import Navigation from './assets/Components/Navigation';
 import Landing from './assets/Components/Landing';
@@ -28,23 +28,23 @@ class App extends Component {
       awards : profileData.awards
     }
   }
-  // sleep = milliseconds => {
-  //   return new Promise(resolve => setTimeout(resolve, milliseconds));
-  // };
-  // wait = async (milliseconds = 2000) => {
-  //   await this.sleep(milliseconds);
-  //   this.setState({
-  //     loading: false
-  //   });
-  // };
+  sleep = milliseconds => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+  };
+  wait = async (milliseconds = 2000) => {
+    await this.sleep(milliseconds);
+    this.setState({
+      loading: false
+    });
+  };
 
-  // componentDidMount() {
-  //   this.wait(2000);
-  //   // this.fetchGitHub();
-  // }
+  componentDidMount() {
+    this.wait(2000);
+    // this.fetchGitHub();
+  }
 
   render() {
-    // if (this.state.loading) return<Loader/>
+    if (this.state.loading) return<Loader/>
     
     return (
       <div className="App">
