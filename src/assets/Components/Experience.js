11d0428a70c78c252879f089aed898ae18b0b894
jacksonmoji <jacksonmoji@gmail.com>
2020-06-25 03:10:03 +0200

    added fix to experience section

--- src/assets/Components/Experience.js
+++ src/assets/Components/Experience.js
@@ -29,7 +29,14 @@ class Experience extends Component {
               <h1>THOUGHT EXPRESS</h1>
               <h2>SOFWARE ENGINEER | BUSINESS PROCESS ENGINEER</h2>
               <h3>01/2019 - 11/2019</h3>
-              <p className="paragraph">Successfully finished leading data migration/conversion from client's Oracle DBMS to Thought Express's System. Delivering the milestones before the deadlines. We believe in under-promising and over-delivering. Below are the activities performed in my role: Lead the business requirements analysis, design, development and quality assurance of technical/business solutions. Reviewed and re-designed existing processes to provide faster and more effective solutions. Engaged with clients in understanding the particular business needs/problems they needed to solve in relation to their business processes. Combined analytical thinking with process understanding to meet and solve problems. Collaborated internally with the project team to design and build the proposed business process solution. Worked individually towards my own deadlines while simultaneously engaging with the team in achieving collective goals and deadlines.</p>
+              <p className="paragraph">
+                Led data migration/conversion from client’s Oracle DBMS to Thought Express’s System.
+                Lead the business requirements analysis, design, development and quality assurance of technical/business solutions.
+                Reviewed and re-designed existing processes to provide faster and more effective solutions.
+                Engaged with clients in understanding the particular business needs/problems they needed to solve in relation to their business processes.
+                Combined analytical thinking with process understanding to meet and solve problems.
+                Collaborated internally with the project team to design and build the proposed business process solution.
+              </p>
             </div>
           </div>
           <div class="timeline__container timeline__container--left">
@@ -37,7 +44,13 @@ class Experience extends Component {
               <h1>SQLI</h1>
               <h2>WEB DEVELOPER</h2>
               <h3>06/2018 - 01/2019</h3>
-              <p className="paragraph">Developed extensions and applications in PHP Developed integrations from 3rd party systems into our systems Performed code reviews and design reviews from team members Provided input regarding new web technologies Performed Troubleshooting and fixed problems Reviewed and participated in testing Worked with customer service to correct issues identified by users</p>
+              <p className="paragraph">
+                Developed extensions and applications in PHP for Magento E-Commerce apps built for clients.
+                Performed code reviews and design reviews from team members.
+                Performed Troubleshooting and fixed tickets issued in Jira.
+                Wrote and reviewed team mates' PHP unit tests.
+                Worked with customer service to correct issues identified by users.
+              </p>
             </div>
           </div>
           <div class="timeline__container timeline__container--right">
@@ -45,7 +58,9 @@ class Experience extends Component {
               <h1>OPEN SOFTWARE</h1>
               <h2>JUNIOR WEB DEVELOPER</h2>
               <h3>06/2015 - 03/2017</h3>
-              <p className="paragraph">Successfully finished leading data migration/conversion from client's Oracle DBMS to Thought Express's System. Delivering the milestones before the deadlines. We believe in under-promising and over-delivering. Below are the activities performed in my role: Lead the business requirements analysis, design, development and quality assurance of technical/business solutions. Reviewed and re-designed existing processes to provide faster and more effective solutions. Engaged with clients in understanding the particular business needs/problems they needed to solve in relation to their business processes. Combined analytical thinking with process understanding to meet and solve problems. Collaborated internally with the project team to design and build the proposed business process solution. Worked individually towards my own deadlines while simultaneously engaging with the team in achieving collective goals and deadlines.</p>
+              <p className="paragraph">
+                Designed and developed a Field Technician’s Service Management Software using Laravel, VueJs, Bootstrap, and Gitlab for Gaborone Electronics. Maintained an Insurance Management System Developed using Symphony, JavaScript, and Bootstrap for a FSG Botswana.
+              </p>
             </div>
           </div>
         </div>
