b7ec2033d112fa8b06c63d42bbefa772991754b3
Jackson Moji <jacksonmoji@gmail.com>
2020-10-16 23:32:02 +0200

    Update Portfolio.js

--- src/assets/Components/Portfolio.js
+++ src/assets/Components/Portfolio.js
@@ -25,7 +25,7 @@ class Portfolio extends Component {
                 <img src={project_p1} alt="bantuweb"></img>
               </div>
               <div className="file__side file__side--back file__side--back-1">
-                <a href="http://ec2-52-87-247-222.compute-1.amazonaws.com/" className="btn btn--blue btn--animated"><FaExternalLinkAlt /></a>
+                <a href="http://bit.ly/bantuweb" className="btn btn--blue btn--animated"><FaExternalLinkAlt /></a>
                 <a href="#" className="btn btn--dark btn--animated"><FaGithub /> </a>
               </div>
             </div>
